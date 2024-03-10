7e6eceb324ea12cd46541759268c1af83490a7d6
Lucas McDonald <lucmcd10@gmail.com>
2024-03-10 18:44:07 -0400

    styling for apply damage

--- src/components/hpCell.tsx
+++ src/components/hpCell.tsx
@@ -13,22 +13,23 @@ function HpPopoverContent({
 }) {
   const [inputVal, setInputVal] = useState<string>("");
   return (
-    <PopoverContent>
-      <div>Apply Damage</div>
-      <form id="apply-damage-form" onSubmit={(e) => {
-        e.preventDefault();
-        if (isNaN(parseInt(inputVal ?? ""))) return;
-          applyDamage(parseInt(inputVal ?? 0));
-      }}>
-        <Input
-          type="number"
-          form="apply-damage-form"
-          value={inputVal ?? ""}
-          onChange={(e) => setInputVal(e.target.value)}
-          className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-        />
-      </form>
-      
+    <PopoverContent className="min-w-0 w-full">
+      <span className="flex gap-2 font-light items-center">
+        Apply Damage
+        <form id="apply-damage-form" onSubmit={(e) => {
+          e.preventDefault();
+          if (isNaN(parseInt(inputVal ?? ""))) return;
+            applyDamage(parseInt(inputVal ?? 0));
+        }}>
+          <Input
+            type="number"
+            form="apply-damage-form"
+            value={inputVal ?? ""}
+            onChange={(e) => setInputVal(e.target.value)}
+            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none w-12"
+          />
+        </form>
+      </span>
     </PopoverContent>
   );
 }
@@ -41,6 +42,7 @@ export function HpCell({
   currentHp: number;
   maxHp: number;
   applyDamage: (amount: number) => void;
+  
 }) {
   const [isOpen, setIsOpen] = useState(false);
   const popoverRef = useRef(null);
@@ -53,7 +55,6 @@ export function HpCell({
         applyDamage={(amount) => {
           applyDamage(amount);
           setIsOpen(false);
-          window.focus();
         }}
       />
     </Popover>
